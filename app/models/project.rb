9de39c40bf886363e4b54a8344f35fa7da4e9de4
Luca Candela <ilcande_prime@live.it>
2025-02-20 16:40:11 +0100

    define model associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  STATUSES = %w[Pending In\ Progress Review Completed Archived].freeze
+  validates :status, inclusion: { in: STATUSES }
+  validates :name, presence: true
 end
